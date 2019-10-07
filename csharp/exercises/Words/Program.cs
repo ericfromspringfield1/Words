98706a23b235c674b302f2108e13054840168ed7
Eric Taylor <ericfromspringfield@gmail.com>
2019-10-07 15:26:53 -0500

    Dictionary of Words and List of Dictionaries About Words

--- csharp/exercises/Words/Program.cs
+++ csharp/exercises/Words/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 
 namespace Words
-{    class Program
+{
+    class Program
     {
         static void Main(string[] args)
         {
@@ -10,36 +11,63 @@ namespace Words
     Create a dictionary with key value pairs to
     represent words (key) and its definition (value)
 */
-Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
 
-// Add several more words and their definitions
-wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
-wordsAndDefinitions.Add("Stank", "The feeling of students when they get all up in the valley of despair");
-wordsAndDefinitions.Add("Woot", "The sound of excitement when a student whoops C#");
-wordsAndDefinitions.Add("Poot", "The sound of gas leaving the bootwah");
-wordsAndDefinitions.Add("Question", "What students should ask many of when at NSS");
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            wordsAndDefinitions.Add("Stank", "The feeling of students when they get all up in the valley of despair");
+            wordsAndDefinitions.Add("Woot", "The sound of excitement when a student whoops C#");
+            wordsAndDefinitions.Add("Poot", "The sound of gas leaving the bootwah");
+            wordsAndDefinitions.Add("Question", "What students should ask many of when at NSS");
 
-/*
-    Use square brackets to get the definition of two of the
-    words and then output them to the console
-*/
+            /*
+                Use square brackets to get the definition of two of the
+                words and then output them to the console
+            */
 
-string def1 = wordsAndDefinitions["Awesome"];
-string def2 = wordsAndDefinitions["Question"];
+            string def1 = wordsAndDefinitions["Awesome"];
+            string def2 = wordsAndDefinitions["Question"];
 
-Console.WriteLine(def1);
-Console.WriteLine(def2);
+            /*
+                Below, loop over the wordsAndDefinitions dictionary to get the following output:
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+                // Console.WriteLine($"The definition of {word.Key} is {word.Value}");
+            }
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "pumped about stuff");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I'm excited about stuff");
 
-/*
-    Below, loop over the wordsAndDefinitions dictionary to get the following output:
-        The definition of (WORD) is (DEFINITION)
-        The definition of (WORD) is (DEFINITION)
-        The definition of (WORD) is (DEFINITION)
-*/
-foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
-{
-// Console.WriteLine($"The definition of {word.Key} is {word.Value}");
-}
+            dictionaryOfWords.Add(excitedWord);
+
+            Dictionary<string, string> sharpWord = new Dictionary<string, string>();
+            sharpWord.Add("word", "sharp");
+            sharpWord.Add("definition", "Cuts like a C");
+            sharpWord.Add("part of speech", "adjective");
+            sharpWord.Add("example sentence", "This C over here is sharp.");
+
+            dictionaryOfWords.Add(sharpWord);
+
+            foreach (Dictionary<string, string> word in dictionaryOfWords)
+            {
+                foreach (KeyValuePair<string, string> kvp in word)
+                {
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }
+            }
+
+
+
+
         }
     }
 }
+
